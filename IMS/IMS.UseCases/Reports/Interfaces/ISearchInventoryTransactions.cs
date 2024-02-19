ee9d972361070a4c9371237237c07afd54f3d66a
AngeloAppec <angelo@appec.nl>
2024-02-19 16:59:31 +0100

    initial push

--- IMS/IMS.UseCases/Reports/Interfaces/ISearchInventoryTransactions.cs
+++
@@ -1,9 +0,0 @@
-﻿using IMS.CoreBusiness;
-
-namespace IMS.UseCases.Reports.Interfaces
-{
-    public interface ISearchInventoryTransactions
-    {
-        Task<IEnumerable<InventoryTransaction>> executeAsync(string inventoryName, DateTime? datefrom, DateTime? dateTo, InventoryTransactionType? transactionType);
-    }
-}
